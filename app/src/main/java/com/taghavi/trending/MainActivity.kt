bb30f1ecdf2997c72a37b3a1b3aebc4e41ff5429
Ehsan Taghavi <ehsantgv@gmail.com>
2020-11-23 23:29:35 +0330

    MainActivity adjustments for Fragment

--- app/src/main/java/com/taghavi/trending/MainActivity.kt
+++ app/src/main/java/com/taghavi/trending/MainActivity.kt
@@ -2,10 +2,20 @@ package com.taghavi.trending
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+        NavigationUI.setupActionBarWithNavController(
+            this,
+            findNavController(R.id.main_nav_fragment)
+        )
     }
+
+    override fun onSupportNavigateUp() = findNavController(R.id.main_nav_fragment).navigateUp()
 }
