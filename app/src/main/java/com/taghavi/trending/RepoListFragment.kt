e3cc94b67d23a4cbb76d07a88c17b32d2dc8662a
Ehsan Taghavi <ehsantgv@gmail.com>
2020-11-24 22:28:49 +0330

    RepoListFragment add dataBinding

--- app/src/main/java/com/taghavi/trending/RepoListFragment.kt
+++ app/src/main/java/com/taghavi/trending/RepoListFragment.kt
@@ -5,13 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProviders
+import com.taghavi.trending.databinding.FragmentRepoListBinding
 
 class RepoListFragment : Fragment() {
+    private lateinit var viewDataBinding: FragmentRepoListBinding
+//    private lateinit var adapter: RepoListAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_repo_list, container, false)
+    ): View {
+        viewDataBinding = FragmentRepoListBinding.inflate(inflater, container, false).apply {
+//            viewmodel =                ViewModelProviders.of(this@RepoListFragment).get(RepoListViewModel::class.java)
+            lifecycleOwner = viewLifecycleOwner
+        }
+        return viewDataBinding.root
     }
 }
