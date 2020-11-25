a7d8daa08d99faa311918de9c87fad1af6b758fb
EhsanTgv <EhsanTgv@gmail.com>
2020-11-25 23:49:14 +0330

    Implement RepoListAdapter class

---
+++ app/src/main/java/com/taghavi/trending/RepoListAdapter.kt
@@ -0,0 +1,28 @@
+package com.taghavi.trending
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.taghavi.trending.RepoListViewHolder
+import com.taghavi.trending.RepoListViewModel
+
+class RepoListAdapter(private val repoListViewModel: RepoListViewModel) : RecyclerView.Adapter<RepoListViewHolder>() {
+    var repoList: List<Item> = emptyList()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RepoListViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val dataBinding = ViewRepoListItemBinding.inflate(inflater, parent, false)
+        return RepoListViewHolder(dataBinding, repoListViewModel)
+    }
+
+    override fun getItemCount() = repoList.size
+
+    override fun onBindViewHolder(holder: RepoListViewHolder, position: Int) {
+        holder.setup(repoList[position])
+    }
+
+    fun updateRepoList(repoList: List<Item>) {
+        this.repoList = repoList
+        notifyDataSetChanged()
+    }
+}
